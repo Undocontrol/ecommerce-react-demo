6c8be97a1337561f11bf923be65b2232339479f2
Emma P <undocontrol88@gmail.com>
2019-10-08 11:28:32 +0100

    Uninstalls moxios as it's overkill..cleans up component folder structure

--- src/components/CardDetails/CardDetails.test.js
+++ src/components/CardDetails/CardDetails.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CardDetails from './CardDetails';
+import CardDetails from '../CardDetails';
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { findByTestAttr } from '../test/testUtils.js';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props={}) => {
+  return shallow(<Instructions {...props}/>)
+};
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
